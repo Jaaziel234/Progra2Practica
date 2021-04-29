85b94c1904b6cccc17133e9e9e2fcf5dbc6c1f2a
Jaaziel234 <63311578+Jaaziel234@users.noreply.github.com>
2021-04-29 15:48:23 -0600

    Update bd.sql

--- practica/BD/bd.sql
+++ practica/BD/bd.sql
@@ -12,6 +12,6 @@ CREATE TABLE usuarios(
     token VARCHAR(10) DEFAULT NULL
 );
 
-INSERT INTO usuarios(usuario,passw,tipo,estado,token) VALUES('jose','$2y$10$ObYjfvb7iKe5Y7CrhCohpucn/whdzwY8c5DcNYiH7LoaJ7e/RYq8u','1','1',NULL);
+INSERT INTO usuarios(usuario,passw,tipo,estado,token) VALUES('jaaziel','$2y$10$ObYjfvb7iKe5Y7CrhCohpucn/whdzwY8c5DcNYiH7LoaJ7e/RYq8u','1','1',NULL);
 
 SELECT * FROM usuarios;
